4081d7ae6875ccc3d02e548bbceb1da340b95fc7
Katya Popova <Katya_ Popova@mail.ru>
2023-03-02 19:21:33 +0300

    дети

---
+++ Child.java
@@ -0,0 +1,16 @@
+
+public class Child extends Human
+{   
+
+
+    public Child(String name, String sex, int age, String wife, String grandfather, String grandmother) 
+    {
+        super(name, sex, age, wife, grandfather, grandmother);
+    }
+
+    @Override
+    public String toString() 
+    {
+        return super.toString();
+    }
+}
